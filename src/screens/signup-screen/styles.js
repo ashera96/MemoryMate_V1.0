d8d98920a939e5520c0a95bb05bb193e12f92e87
Ashera Silva <asherasilva100@gmail.com>
2019-03-15 19:33:32 +0530

    Added styling for the signupScreen page

---
+++ src/screens/signup-screen/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "white"
+  },
+  inputItem: {
+    marginTop: 20,
+    height: 50,
+    backgroundColor: "rgba(180,180,225,0.4)"
+  },
+  inputBox: {
+    color: "black"
+  },
+  icon: {
+    marginHorizontal: 12,
+    color: "white"
+  },
+  buttonItem: {
+    marginLeft: 0,
+    marginTop: 35,
+    borderBottomWidth: 0
+  },
+  button: {
+    height: 50
+  },
+  textItem: {
+    marginLeft: 0,
+    marginTop: 30,
+    borderBottomWidth: 0
+  }
+});
+
+export { styles };
