050536aa47d1ef2480269321c0bcba86f7fbaf12
Ashera Silva <asherasilva100@gmail.com>
2019-03-30 11:15:54 +0530

    Added styling for the add_category page

---
+++ src/screens/add-category-screen/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  // styles here
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  headerText: {
+    fontSize: 12, 
+  },
+  inputField: {
+    paddingBottom: 20,
+    paddingLeft: 10,
+    color: 'rgba(48, 48, 48, 0.9)',
+  },
+  inputItem: {
+    marginTop: 2,
+    backgroundColor: "rgba(180,180,225,0.4)"
+  },
+});
+
+export { styles };
