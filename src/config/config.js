a012dec213ca4172f80b25dd9345cc92084d8146
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-03-30 11:37:43 +0530

    Connected firestore database

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+// firebase configurations
+const firebaseConfig = {
+  apiKey: "AIzaSyAJiNBrEJ4tF_lLezf-w-zqIpgBmLcjBYg",
+  authDomain: "memorymate-61d4b.firebaseapp.com",
+  databaseURL: "https://memorymate-61d4b.firebaseio.com",
+  projectId: "memorymate-61d4b",
+  storageBucket: "memorymate-61d4b.appspot.com",
+  messagingSenderId: "1027977346482"
+};
+
+export { firebaseConfig };
